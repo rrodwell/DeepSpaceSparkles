88ae6a5347e674bd142cf02d642b6f4508b33bd6
Ryan Rodwell <ryanrodwell@lawn-128-61-63-54.lawn.gatech.edu>
2017-03-14 20:37:01 -0400

    initializing firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyB7C05zBpbSGSUEAabZFNOiUPjX0ZYM9xI",
+   authDomain: "beerme-73beb.firebaseapp.com",
+   databaseURL: "https://beerme-73beb.firebaseio.com",
+   storageBucket: "beerme-73beb.appspot.com",
+   messagingSenderId: "364897926231"
+ };
+ firebase.initializeApp(config);
