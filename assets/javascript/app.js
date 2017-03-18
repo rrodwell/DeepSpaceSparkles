1d6b0d0d7c182420502faec575362c9fc760b6c8
SUNGBUM PARK <SUNGBUM PARK>
2017-03-18 13:46:43 -0400

    updating js file with click functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,15 @@
 // Initialize Firebase
+ var fStandard = 273.15; 
+ var windPower = 0 ;
+ var code = "";
+ var WindStandard = 10.8 ;
+ var waryhumidity = 50;
+ var normalhumidity = 40 ;  
+ var baldTest =false;
+ var minTemperature = 285.95;
+ var maxTemperature = 297.59;
+
+
  var config = {
    apiKey: "AIzaSyB7C05zBpbSGSUEAabZFNOiUPjX0ZYM9xI",
    authDomain: "beerme-73beb.firebaseapp.com",
@@ -8,66 +19,110 @@
  };
  firebase.initializeApp(config);
 
+ 
+
+
+
+   function mainTask(Humidity){
 //var wCondition = ["shower rain", "rain" , "thunderstorm" , "snow" , "mist" ,"clear sky", "few clouds","scattered clouds", "broken clouds"]; 
-var fStandard = 273.15; 
-var windPower = 0 ;
-var code = "";
-var WindStandard = 10.8 ;
-var stonghumidity = 70; 
- // API key for open weather
- var APIKey = "da2fd5126cda625878969d8aa3d25d93";
+         
+         // API key for open weather
+           var code = ""; 
 
- navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position);
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    
+           var testHuidity = Humidity; 
+            
+         
+           var APIKey = "da2fd5126cda625878969d8aa3d25d93";
 
-   // Here we are building the URL we need to query the database
-   var queryURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid="+APIKey;
-   // Here we run our AJAX call to the OpenWeatherMap API
-   $.ajax({
-       url: queryURL,
-       method: "GET"
-     })
-     // We store all of the retrieved data inside of an object called "response"
-     .done(function(response) {
+           navigator.geolocation.getCurrentPosition(function(position) {
+           
+           
+            console.log(position);
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            
 
-       
-       
-       if( response.main.temp <= fStandard){
-         code = code+"1";   
-       }else{
-         code = code+"0";
-       }
+           // Here we are building the URL we need to query the database
+           var queryURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid="+APIKey;
+           // Here we run our AJAX call to the OpenWeatherMap API
+           $.ajax({
+               url: queryURL,
+               method: "GET"
+             })
+             // We store all of the retrieved data inside of an object called "response"
+             .done(function(response) {
 
-       if( response.wind.speed >= WindStandard){
-         code = code+"1";   
-       }else{
-         code = code+"0";
-       }
-
-        if( response.main.humidity >= stonghumidity){
-         code = code+"1";   
-       }else{
-         code = code+"0";
-       }
+               
+               if(response.main.humidity > testHuidity   ){
+               code = code + "H";}
+               else{code = code + "X";} 
 
 
-       
-       console.log(code);
-       // Transfer content to HTML
-       $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-       $(".wind").html("Wind Speed: " + response.wind.speed);
-       $(".humidity").html("Humidity: " + response.main.humidity);
-       $(".temp").html("Temperature (F) " + response.main.temp);
 
-       // Log the data in the console as well
-       console.log("Wind Speed: " + response.wind.speed);
-       console.log("Humidity: " + response.main.humidity);
-       console.log("Temperature (F): " + response.main.temp);
-     });
+               if(response.wind.speed > WindStandard  ){
+               code = code + "W";}
+               else{code = code + "N";} 
+
+
+               if(response.main.temp < minTemperature  ){
+               code = code + "1";
+               }else if ( response.main.temp > maxTemperature){
+               code = code + "3";
+               }else{ 
+               code = code + "2";
+               }
+               
+              
+               console.log("code : " + code ); 
+               // Log the data in the console as well
+               console.log("Wind Speed: " + response.wind.speed);
+               console.log("Humidity: " + response.main.humidity);
+               console.log("Temperature (F): " + response.main.temp);
+             });
+           
+
+
+           });
+
+   }
+
+ $( document ).on( 'click', '#yesButton', function () {
+   console.log("Choose Your Hairstyle");
+
+   baldTest = false ; 
+   console.log(baldTest );
+
+ });
+  $( document ).on( 'click', '#noButton', function () {
+   console.log("I am so sorry....... ");
+    baldTest = true ; 
+    console.log(baldTest );
+
+ });
+
+   $( document ).on( 'click', '#CurlyButton', function () {
+    if( baldTest == false ){
+     mainTask(40);
+    }else{
+     alert("You are bald. So you don't need my advice! "); 
+    }
    
 
+ });
+    $( document ).on( 'click', '#wavy', function () {
+     if( baldTest == false ){
+     mainTask(50);
+    }else{
+     alert("You are bald. So you don't need my advice! "); 
+    }
+   
 
-   });
+ });
+     $( document ).on( 'click', '#Straight', function () {
+     if( baldTest == false ){
+     mainTask(40);
+     }else{
+     alert("You are bald. So you don't need my advice! "); 
+     }
+
+ });
