1d9273f487ea5d903fe95d8cc867787514391644
Ryan Rodwell <ryanrodwell@lawn-128-61-49-109.lawn.gatech.edu>
2017-03-21 19:00:47 -0400

    correcting firebase initialize

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,47 @@
 // Initialize Firebase
- var fStandard = 273.15; 
+ var fStandard = 273.15;
  var windPower = 0 ;
  var code = "";
  var WindStandard = 10.8 ;
  var waryhumidity = 50;
- var normalhumidity = 40 ;  
+ var normalhumidity = 40 ;
  var baldTest =false;
  var minTemperature = 285.95;
  var maxTemperature = 297.59;
 
 
- var config = {
-   apiKey: "AIzaSyB7C05zBpbSGSUEAabZFNOiUPjX0ZYM9xI",
-   authDomain: "beerme-73beb.firebaseapp.com",
-   databaseURL: "https://beerme-73beb.firebaseio.com",
-   storageBucket: "beerme-73beb.appspot.com",
-   messagingSenderId: "364897926231"
- };
- firebase.initializeApp(config);
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyASY7NBwl2uKMKjmaQ-docJJAEzhz0N4tg",
+    authDomain: "stylecast-be501.firebaseapp.com",
+    databaseURL: "https://stylecast-be501.firebaseio.com",
+    storageBucket: "stylecast-be501.appspot.com",
+    messagingSenderId: "911214775223"
+  };
+  firebase.initializeApp(config);
 
- 
+
 
 
 
    function mainTask(Humidity){
-//var wCondition = ["shower rain", "rain" , "thunderstorm" , "snow" , "mist" ,"clear sky", "few clouds","scattered clouds", "broken clouds"]; 
-         
-         // API key for open weather
-           var code = ""; 
+//var wCondition = ["shower rain", "rain" , "thunderstorm" , "snow" , "mist" ,"clear sky", "few clouds","scattered clouds", "broken clouds"];
 
-           var testHuidity = Humidity; 
-            
-         
+         // API key for open weather
+           var code = "";
+
+           var testHuidity = Humidity;
+
+
            var APIKey = "da2fd5126cda625878969d8aa3d25d93";
 
            navigator.geolocation.getCurrentPosition(function(position) {
-           
-           
+
+
             console.log(position);
             var lat = position.coords.latitude;
             var lng = position.coords.longitude;
-            
+
 
            // Here we are building the URL we need to query the database
            var queryURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid="+APIKey;
@@ -52,34 +53,34 @@
              // We store all of the retrieved data inside of an object called "response"
              .done(function(response) {
 
-               
+
                if(response.main.humidity > testHuidity   ){
                code = code + "H";}
-               else{code = code + "X";} 
+               else{code = code + "X";}
 
 
 
                if(response.wind.speed > WindStandard  ){
                code = code + "W";}
-               else{code = code + "N";} 
+               else{code = code + "N";}
 
 
                if(response.main.temp < minTemperature  ){
                code = code + "1";
                }else if ( response.main.temp > maxTemperature){
                code = code + "3";
-               }else{ 
+               }else{
                code = code + "2";
                }
-               
-              
-               console.log("code : " + code ); 
+
+
+               console.log("code : " + code );
                // Log the data in the console as well
                console.log("Wind Speed: " + response.wind.speed);
                console.log("Humidity: " + response.main.humidity);
                console.log("Temperature (F): " + response.main.temp);
              });
-           
+
 
 
            });
@@ -89,13 +90,13 @@
  $( document ).on( 'click', '#yesButton', function () {
    console.log("Choose Your Hairstyle");
 
-   baldTest = false ; 
+   baldTest = false ;
    console.log(baldTest );
 
  });
   $( document ).on( 'click', '#noButton', function () {
    console.log("I am so sorry....... ");
-    baldTest = true ; 
+    baldTest = true ;
     console.log(baldTest );
 
  });
@@ -104,25 +105,25 @@
     if( baldTest == false ){
      mainTask(40);
     }else{
-     alert("You are bald. So you don't need my advice! "); 
+     alert("You are bald. So you don't need my advice! ");
     }
-   
+
 
  });
     $( document ).on( 'click', '#wavy', function () {
      if( baldTest == false ){
      mainTask(50);
     }else{
-     alert("You are bald. So you don't need my advice! "); 
+     alert("You are bald. So you don't need my advice! ");
     }
-   
+
 
  });
      $( document ).on( 'click', '#Straight', function () {
      if( baldTest == false ){
      mainTask(40);
      }else{
-     alert("You are bald. So you don't need my advice! "); 
+     alert("You are bald. So you don't need my advice! ");
      }
 
  });
