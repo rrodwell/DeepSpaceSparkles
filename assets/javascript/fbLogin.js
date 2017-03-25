2944833308f549f659275dac586ff8c206c79916
Ryan Rodwell <ryanrodwell@gmail.com>
2017-03-25 01:02:08 -0400

    adding fbLogin js file

--- assets/javascript/fbLogin.js
+++ assets/javascript/fbLogin.js
@@ -10,10 +10,7 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
         testAPI();
-    } else {
-        // The person is not logged into your app or we are unable to tell.
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
+        window.location.href = "hairQuestions.html"
     }
 }
 
